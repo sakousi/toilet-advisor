4c80bfc53efe0d02461be6fab741ee41e92a5012
theau <theaudugueperoux@gmail.com>
2023-09-20 11:44:38 +0200

    :card_file_box: update City related object + add adders in model.php

--- src/classes/City.php
+++ src/classes/City.php
@@ -4,10 +4,20 @@ class City{
     private $id = 0;
     private $name = '';
     private $zipCode = '';
+    private $toilets = array();
+
 
     public function __construct($name, $zipCode) {
-        $this->name = $name;
-        $this->zipCode = $zipCode;
+        //set city by the database and join with the toilets table
+        global $bdd;
+        $req = $bdd->prepare('SELECT * FROM cities INNER JOIN toilets ON cities.id = toilets.city_id WHERE cities.name = ? AND cities.zip_code = ?');
+        $req->execute(array($name, $zipCode));
+        $city = $req->fetch();
+        $this->id = $city['id'];
+        $this->name = $city['name'];
+        $this->zipCode = $city['zip_code'];
+        $this->toilets = $city['toilets']; //array of toilets ids
+
     }
 
     //getters and setters
@@ -15,22 +25,16 @@ class City{
     public function getId() {
         return $this->id;
     }
-
-    public function setId($id) {
-        if (!is_numeric($id)) {
-            throw new Exception('ID doit être un nombre');
-        }
-        $this->id = $id;
-    }
     
     public function getName() {
         return $this->name;
     }
 
     public function setName($name) {
-        if (strlen($name) < 3) {
-            throw new Exception('Nom trop court');
-        }
+        //update name in database
+        global $bdd;
+        $req = $bdd->prepare('UPDATE cities SET name = ? WHERE id = ?');
+        $req->execute(array($name, $this->id));
         $this->name = $name;
     }
 
@@ -39,10 +43,31 @@ class City{
     }
 
     public function setZipCode($zipCode) {
-        if (strlen($zipCode) < 3) {
-            throw new Exception('Pays trop court');
-        }
+        //update zipCode in database
+        global $bdd;
+        $req = $bdd->prepare('UPDATE cities SET zip_code = ? WHERE id = ?');
+        $req->execute(array($zipCode, $this->id));
         $this->zipCode = $zipCode;
+    }
+
+    public function getToilets() {
+        return $this->toilets;
+    }
+
+    public function addToilets($toiletId) {
+        //add toilet in database
+        global $bdd;
+        $req = $bdd->prepare('INSERT INTO toilets(city_id) VALUES(?)');
+        $req->execute(array($toiletId));
+        $this->toilets[] = $toiletId;
+    }
+
+    public function deleteToilets($toiletId) {
+        //delete toilet in database
+        global $bdd;
+        $req = $bdd->prepare('DELETE FROM toilets WHERE id = ?');
+        $req->execute(array($toiletId));
+        $this->toilets = array_diff($this->toilets, array($toiletId));
     }
 
 }
