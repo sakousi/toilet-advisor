80835003c41c6b26c17c6f09261cd90cd1b433cb
theau <theaudugueperoux@gmail.com>
2023-09-20 11:33:23 +0200

    :card_file_box: updated Toilet related object

--- src/classes/toilet.php
+++ src/classes/toilet.php
@@ -1,28 +1,31 @@
 <?php
+require 'src/model.php';
 class Toilet{
 
     private $id = 0;
     private $address = '';
     private $accessibility = '';
     private $cleanliness = '';
+    private $city = '';
 
-    public function __construct($address, $accessibility, $cleanliness) {
-        $this->address = $address;
-        $this->accessibility = $accessibility;
-        $this->cleanliness = $cleanliness;
+    public function __construct($toiletId) {
+        //get toilet from database with a join in order to get the city name
+        global $bdd;
+        $req = $bdd->prepare('SELECT * FROM toilets INNER JOIN cities ON toilets.city_id = cities.id WHERE toilets.id = ?');
+        $req->execute(array($toiletId));
+        $toilet = $req->fetch();
+        $this->id = $toilet['id'];
+        $this->address = $toilet['address'];
+        $this->accessibility = $toilet['accessibility'];
+        $this->cleanliness = $toilet['cleanliness'];
+        $this->city = $toilet['name'];
+
     }
 
     //getters and setters
 
     public function getId() {
         return $this->id;
-    }
-
-    public function setId($id) {
-        if (!is_numeric($id)) {
-            throw new Exception('ID doit être un nombre');
-        }
-        $this->id = $id;
     }
 
     public function getAddress() {
@@ -33,7 +36,56 @@ class Toilet{
         if (strlen($address) < 3) {
             throw new Exception('Adresse trop courte');
         }
+        //update address in database
+        global $bdd;
+        $req = $bdd->prepare('UPDATE toilets SET address = ? WHERE id = ?');
+        $req->execute(array($address, $this->id));
         $this->address = $address;
+    }
+
+    public function getAccessibility() {
+        return $this->accessibility;
+    }
+
+    public function setAccessibility($accessibility) {
+        if (strlen($accessibility) < 3) {
+            throw new Exception('Accessibilité trop courte');
+        }
+        //update accessibility in database
+        global $bdd;
+        $req = $bdd->prepare('UPDATE toilets SET accessibility = ? WHERE id = ?');
+        $req->execute(array($accessibility, $this->id));
+        $this->accessibility = $accessibility;
+    }
+
+    public function getCleanliness() {
+        return $this->cleanliness;
+    }
+
+    public function setCleanliness($cleanliness) {
+        if (strlen($cleanliness) < 3) {
+            throw new Exception('Propreté trop courte');
+        }
+        //update cleanliness in database
+        global $bdd;
+        $req = $bdd->prepare('UPDATE toilets SET cleanliness = ? WHERE id = ?');
+        $req->execute(array($cleanliness, $this->id));
+        $this->cleanliness = $cleanliness;
+    }
+
+    public function getCity() {
+        return $this->city;
+    }
+
+    public function setCity($city) {
+        if (strlen($city) < 3) {
+            throw new Exception('Ville trop courte');
+        }
+        //update city in database
+        global $bdd;
+        $req = $bdd->prepare('UPDATE toilets SET city = ? WHERE id = ?');
+        $req->execute(array($city, $this->id));
+        $this->city = $city;
     }
 
 }
