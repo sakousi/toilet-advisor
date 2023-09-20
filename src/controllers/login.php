3f39465b4feeb11724b2b98ca428a662a6794f39
Samuel Alleaume <samuel@alleaume.eu>
2023-09-20 10:02:06 +0200

    Register / Login .php templates and controllers

---
+++ src/controllers/login.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once('src/model.php');
+
+function login() {
+	require('templates/login.php');
+}
---
+++ src/controllers/register.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once('src/model.php');
+
+function register() {
+	require('templates/register.php');
+}
