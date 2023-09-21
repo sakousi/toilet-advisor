c317cedcea73c627d5474afa79f333dbdb774fc0
Samuel Alleaume <samuel@alleaume.eu>
2023-09-21 09:31:38 +0200

    basic footer frontstyle added

--- templates/component/footer.php
+++ templates/component/footer.php
@@ -1,5 +1,13 @@
-<footer>
-    
+<footer class="bg-blue-500 p-6 text-white">
+  <div class="flex justify-between items-center">
+    <div>
+      <p>&copy; 2023 Toilet Finder</p>
+    </div>
+    <div>
+      <a href="#" class="text-blue-200 hover:text-white mr-4">About Us</a>
+      <a href="#" class="text-blue-200 hover:text-white">Contact Us</a>
+    </div>
+  </div>
 </footer>
 </body>
 </html>
