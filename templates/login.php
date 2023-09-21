22f509bb7bdd28bd23b53f275200a08e52f00c91
Mathieu de Pindray <solargente1508@gmail.com>
2023-09-21 12:06:31 +0200

    :bug: correction du repeat du header sur la page login

--- templates/login.php
+++ templates/login.php
@@ -1,18 +1,4 @@
 <?php include "component/header.php"?>
-<?php
-  if (isset($_GET['action'])) {
-    $action = $_GET['action'];
-    switch ($action) {
-        case 'login':
-            processLogin();
-            break;
-        default:
-            loginController();
-            break;
-    }
-  } else {
-    loginController();
-  } ?>
 
 <form class="flex flex-col items-center" method="post" action="login.php">
   <label for="email" class="text-gray-700 font-bold mb-2">Email:</label>
