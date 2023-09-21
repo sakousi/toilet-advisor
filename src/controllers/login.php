61f11fd7863f20b94c4fd2871cbf55b1ee04d41a
Mathieu de Pindray <solargente1508@gmail.com>
2023-09-21 16:55:15 +0200

    :bug: deleted requires in login and register

--- src/controllers/login.php
+++ src/controllers/login.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once('src/classes/user.php');
-
 function loginController() {
 	require('templates/login.php');
 }
--- src/controllers/register.php
+++ src/controllers/register.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once('src/classes/user.php');
-
 function registerController() {
 	require('templates/register.php');
 }
