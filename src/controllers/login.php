58b617016c1153ff8e7814c5458f80045b72e607
Samuel Alleaume <samuel@alleaume.eu>
2023-09-21 14:21:51 +0200

    Reworking some style around so that it is more readable

--- src/controllers/login.php
+++ src/controllers/login.php
@@ -5,3 +5,43 @@ require_once('src/classes/user.php');
 function loginController() {
 	require('templates/login.php');
 }
+
+
+class LoginController {
+	private $user;
+  
+	public function __construct() {
+	  $this->user = new User();
+	}
+  
+	public function login() {
+		if (isset($_POST['submit'])) {
+			$email = $_POST['email'];
+			$password = $_POST['password'];
+		
+			// Validate the username and password fields
+			if (empty($username)) {
+			$errors['username'] = 'Username is required';
+			}
+			if (empty($password)) {
+			$errors['password'] = 'Password is required';
+			}
+		
+			// If there are no errors, check if the user exists in the database
+			if (empty($errors)) {
+			//Check if the user exists in the database
+			$stmt = $pdo->prepare('SELECT * FROM users WHERE username = :username');
+			$stmt->execute(array('username' => $username));
+			$user = $stmt->fetch();
+		
+			if ($user && password_verify($password, $user['password'])) {
+			  // User exists and password is correct, log them in
+			} else {
+			  // User does not exist or password is incorrect, display an error message
+			}
+			// If the user exists, log them in
+			// Otherwise, display an error message
+			}
+		}
+	}
+}
--- templates/register.php
+++ templates/register.php
@@ -7,10 +7,13 @@
   <input type="email" id="email" name="email" class="w-full px-4 py-2 rounded-l-lg border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white focus:outline-none focus:border-gray-500" placeholder="Email" required>
   <label for="password" class="text-gray-700 font-bold mb-2">Password:</label>
   <input type="password" id="password" name="password" class="w-full px-4 py-2 rounded-l-lg border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white focus:outline-none focus:border-gray-500" placeholder="Password" required>
-  <input type="submit" value="Register" class="px-4 rounded-r-lg bg-gray-800 text-white font-bold p-2 uppercase border-gray-800 border-t border-b border-r hover:bg-gray-700 focus:outline-none focus:bg-gray-700"/>
+  <input type="submit" value="Register" class="mt-4 px-4 rounded-r-lg bg-gray-800 text-white font-bold p-2 uppercase border-gray-800 border-t border-b border-r hover:bg-gray-700 focus:outline-none focus:bg-gray-700"/>
 </form>
 
-<a href="/index" class="px-4 rounded-r-lg bg-gray-800 text-white font-bold p-2 uppercase border-gray-800 border-t border-b border-r hover:bg-gray-700 focus:outline-none focus:bg-gray-700">Retour à la page d'accueil</a>
+<div class="flex flex-col items-center mt-4 mb-4">
+  <a href="/index" class="px-4 rounded-r-lg bg-gray-800 text-white font-bold p-2 uppercase border-gray-800 border-t border-b border-r hover:bg-gray-700 focus:outline-none focus:bg-gray-700">Retour à la page d'accueil</a>
+</div>
+
 
 
 <?php include "component/footer.php" ?>
